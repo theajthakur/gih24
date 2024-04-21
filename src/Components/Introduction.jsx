d4125a3eb25c9d9ca5eff366295be1861434a657
Harshit Sharma <cwd.harshit911@gmail.com>
2024-04-21 13:43:02 +0530

    header and intro done

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import Button from '../UI elements/Button'
-import { Link } from 'react-scroll'
-import laptopImage from '../Assets/Images/laptopimage.png'
+import React from "react";
+import Button from "../UI elements/Button";
+import { Link } from "react-scroll";
+import laptopImage from "../Assets/Images/laptopimage.png";
 
 const Introduction = () => {
   return (
-    <div className='flex justify-center items-center gap-10 min-h-screen d-[900px]' id='intro'>
-      <div className='flex flex-col justify-center '>
-        <div className=' font-mono text-primary text-md'>Hi, my name is</div>
-        <div className=' font-[700] md:text-[70px] text-[40px] font-sans text-lightest_slate leading-[1.2] pb-3'>Shriyam Gera. <br /><span className='text-slate'>I build things for the web.</span></div>
-        <div className=' font-[20px] text-lightest_slate max-w-[580px] mb-8'>I'm a software engineer specializing in <span className='text-primary'>building </span>and
-          <span className='text-primary'> designing </span> exceptional digital experiences.
-          I love to <span className='text-primary'>code</span> and looking for new ideas to
-          implement in real life. Continuously striving to merge creativity with technology to deliver impactful solutions.</div>
-        <Link to='projects' spy={true} smooth={true} duration={500} offset={-100}>
-          <Button title='Register!' />
+    <div
+      className="flex justify-center items-center gap-10 min-h-screen d-[900px]"
+      id="intro"
+    >
+      <div className="flex flex-col justify-center ">
+        <div className="font-mono text-primary text-md">
+          Embrace the challenge
+        </div>
+        <div className=" font-[700] md:text-[70px] text-[40px] font-sans text-lightest_slate">
+          <p>Innohacks 3.0</p>
+          <p className="text-sm text-slate">Hack n' Innovate</p>
+        </div>
+        <div className=" font-[20px] text-lightest_slate max-w-[580px] mb-8">
+          Unleash your creativity for{" "}
+          <span className="text-primary">24-Hour</span>! Join us on{" "}
+          <span className="text-primary">April 28th</span>
+          for a nationwide hackathon alongside hundreds of other{" "}
+          <span className="text-primary">student innovators</span>. Build,
+          learn, and connect - are you in?
+        </div>
+        <Link
+          to="projects"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-100}
+        >
+          <Button title="Register!" />
         </Link>
       </div>
-      <img src={laptopImage} alt='Hackathon' className='rounded-full h-80 w-80 scale-x-[-1] opacity-75 hover:opacity-100 duration-300' />
+      <img
+        src={laptopImage}
+        alt="Hackathon"
+        className="rounded-full h-80 w-80 scale-x-[-1] opacity-75 hover:opacity-100 duration-300"
+      />
     </div>
+  );
+};
 
-  )
-}
-
-export default Introduction
+export default Introduction;
--- src/UI elements/Button.jsx
+++ src/UI elements/Button.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { cn } from "../lib/utils";
 
-const Button = ({title}) => {
+const Button = ({ title, className }) => {
   return (
-    <div className= {`font-mono cursor-pointer ${title === 'Resume' ? 'py-2': null} flex p-4 items-center border-[1px] border-primary text-primary rounded-md  max-w-fit hover:-translate-x-1 hover:-translate-y-1 duration-200 btn_hover_shadow`}>{title}</div>
-  )
-}
+    <div
+      className={cn(
+        `font-mono cursor-pointer flex p-4 items-center border-[1px] border-primary text-primary rounded-md  max-w-fit hover:-translate-x-1 hover:-translate-y-1 duration-200 btn_hover_shadow`,
+        className
+      )}
+    >
+      {title}
+    </div>
+  );
+};
 
-export default Button
+export default Button;
