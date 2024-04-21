fbc44948f7fbfd8ab9a1e7f510fefac3e106b01d
Harshit Sharma <cwd.harshit911@gmail.com>
2024-04-21 14:27:20 +0530

    made prizes section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import './App.css';
-import { AboutMe } from './Components/AboutMe';
-import Contact from './Components/Contact';
-import Experience from './Components/Experience';
-import Footer from './Components/Footer';
-import Introduction from './Components/Introduction';
-import LeftSidebar from './Components/LeftSidebar';
-import Navbar from './Components/Navbar';
-import Projects from './Components/Projects';
-import RightSidebar from './Components/RightSidebar';
-import ParticlesComponent from './Components/Particles';
+import "./App.css";
+import { AboutMe } from "./Components/AboutMe";
+import Contact from "./Components/Contact";
+import Experience from "./Components/Experience";
+import Footer from "./Components/Footer";
+import Introduction from "./Components/Introduction";
+import LeftSidebar from "./Components/LeftSidebar";
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
+import RightSidebar from "./Components/RightSidebar";
+import ParticlesComponent from "./Components/Particles";
+import Prizes from "./Components/Prizes";
 
 function App() {
   return (
-    <div className='custom-scrollbar'>
+    <div className="custom-scrollbar">
       <ParticlesComponent />
       <Navbar />
       <LeftSidebar />
       <RightSidebar />
-      <div className='flex flex-col items-center justify-center'>
+      <div className="flex flex-col items-center justify-center">
         <Introduction />
         <AboutMe />
+        <Prizes />
         <Experience />
         <Projects />
         <Contact />
