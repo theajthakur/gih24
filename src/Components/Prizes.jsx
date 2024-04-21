fb2dc63143f886080cafed0eb67597495d891329
Harshit Sharma <cwd.harshit911@gmail.com>
2024-04-21 23:19:38 +0530

    asdasd

--- src/Components/Prizes.jsx
+++ src/Components/Prizes.jsx
@@ -3,12 +3,15 @@ import Heading from "../UI elements/Heading";
 
 export default function Prizes() {
   return (
-    <div className="flex flex-col justify-center w-full max-w-4xl min-h-[80vh]">
+    <div
+      className="flex flex-col justify-center w-full max-w-4xl min-h-[80vh]"
+      id="prizes"
+    >
       <Heading index={"02"} title={"Prizes"} />
-      <p className="text-lightest_slate font-mono text-3xl my-5">
+      <p className="my-5 font-mono text-3xl text-lightest_slate">
         Prize pool worth <span>₹1.5Lakh</span>
       </p>
-      <div className="flex items-center justify-center w-full rounded-md glassmorphism h-96 text-3xl font-mono">
+      <div className="flex items-center justify-center w-full font-mono text-3xl rounded-md glassmorphism h-96">
         Coming soon.....
       </div>
     </div>
