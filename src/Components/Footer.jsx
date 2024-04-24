71ec6dca2e0b0095bea4df09e064d167a9ca7aff
shriyamgera <64017939+shriyamgera@users.noreply.github.com>
2024-04-24 14:51:02 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <a href='https://chat.whatsapp.com/C7092WXiZNC4200dqEL6fQ' target='_blank' rel="noreferrer"><FaWhatsapp className='   hover:text-primary cursor-pointer duration-300' /></a>
         <a href='https://instagram.com/galgotias_hackathon' target='_blank' rel="noreferrer"><FaInstagram className='   hover:text-primary cursor-pointer duration-300' /></a>
       </div>
-      <a href='https://www.instagram.com/gcetloop' target='_blank' rel="noreferrer"><div className=' lg:text-[30px] flex items-center justify-center font-mono hover:text-primary text-center text-lightest_slate cursor-default duration-300 glassmorphism text-[17px] gap-2 hover:scale-125'><div>Designed & built by </div> <img src='/images/loop.png' alt='loop' className='h-20 mb-1 ' />  </div></a>
+      <a href='https://www.instagram.com/gcetloop' target='_blank' rel="noreferrer"><div className=' lg:text-[30px] flex items-center justify-center font-mono hover:text-primary text-center text-lightest_slate cursor-default duration-300 glassmorphism text-[15px] gap-2 hover:scale-125'><div>Designed & built by </div> <img src='/images/loop.png' alt='loop' className='h-20 mb-1 ' />  </div></a>
       <a href='mailto:galgotiahackathon24@gmail.com' target='_blank' rel="noreferrer"><div className=' font-mono hover:text-primary text-center text-lightest_slate mt-4 cursor-default duration-300 glassmorphism'>© 2024 GalgotiasInternationalHackathon</div></a>
 
     </>
