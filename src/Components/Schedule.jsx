323bde36fd4df0d08f8b467a895c01b4d573cafb
Prerit008 <preritagrawal08@gmail.com>
2024-05-18 13:42:44 +0530

    Schedule Updated 👾

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -3,13 +3,106 @@ import Heading from "../UI elements/Heading";
 
 export default function Schedule() {
   return (
-    <div className="flex flex-col justify-center w-full max-w-4xl min-h-[80vh] glassmorphism text-off_white">
+    <div className="flex flex-col justify-center w-full max-w-4xl min-h-[80vh]  text-off_white">
       <Heading title={"Schedule"} />
       <p className="text-lightest_slate">
       </p>
-      <div className="flex items-center justify-center w-full rounded-md glassmorphism h-60 text-3xl font-mono text-center">
-        Coming Soon.....
-      </div>
+<div class="container glassmorphism">
+          <div
+            class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50 "
+          >
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-primary pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-primary shadow"
+                ></div>
+              </div>
+              <div
+                class="glassmorphism col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">25-26 MAY</h3>
+                <p class="leading-tight text-justify">
+                  HACKATHON ROUND 3 
+                </p>
+              </div>
+            </div>
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="glassmorphism text-white col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">25 MAY</h3>
+                <p class="leading-tight text-justify">
+                  HACKATHON ROUND 2
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-primary pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-primary shadow"
+                ></div>
+              </div>
+            </div>
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-primary pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-primary shadow"
+                ></div>
+              </div>
+              <div
+                class="glassmorphism col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">18-23 MAY</h3>
+                <p class="leading-tight text-justify">
+                  ROUND 2 REGISTRATIONS
+                </p>
+              </div>
+            </div>
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="glassmorphism col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">17-18 MAY</h3>
+                <p class="leading-tight text-justify">
+                  ROUND 1 RESULT
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-primary pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-primary shadow"
+                ></div>
+              </div>
+            </div>
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-primary pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-primary shadow"
+                ></div>
+              </div>
+              <div
+                class="glassmorphism col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">18 MAY</h3>
+                <p class="leading-tight text-justify">
+                  ROUND 1 REGISTRATIONS ENDS
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
