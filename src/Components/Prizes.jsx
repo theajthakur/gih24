8a5ff55ed449a2a295d4e4cb10ebbe265ac81522
Prerit <80686842+Prerit008@users.noreply.github.com>
2024-07-08 14:40:57 +0530

    Update Prizes.jsx

--- src/Components/Prizes.jsx
+++ src/Components/Prizes.jsx
@@ -7,13 +7,22 @@ export default function Prizes() {
       className="flex flex-col justify-center w-full max-w-4xl min-h-[80vh] text-off_white"
       id="prizes"
     >
-      <Heading index={"02"} title={"Prizes"} />
-      <p className="my-5 font-mono text-3xl text-lightest_slate glassmorphism text-center">
-        Prize pool worth <span>₹1.5Lakh</span>
-      </p>
-      <div className="flex items-center justify-center w-full rounded-md glassmorphism h-60 text-3xl font-mono">
-        Coming Soon.....
-      </div>
+      <Heading index={"02"} title={"Our Patrons"} />
+      <div className="grid w-full grid-cols-2">      <div className="text-center text-gray-400 dark:text-gray-400">
+              <img className="mx-auto mb-4 w-36 h-36 rounded-full" src="/images/ceo.jpg"/>
+              <h3 className="mb-1 text-2xl font-bold tracking-tight text-white dark:text-white">
+                  <a href="#">Dr. Dhruv Galgotia</a>
+              </h3>
+              <p>CEO, Galgotia Educational Institutions</p>
+              </div>
+              <div className="text-center text-gray-400 dark:text-gray-400">
+              <img className="mx-auto mb-4 w-36 h-36 rounded-full" src="/images/d.jpeg"/>
+              <h3 className="mb-1 text-2xl font-bold tracking-tight text-white dark:text-white">
+                  <a href="#">MOHD. ASIM QADRI</a>
+              </h3>
+              <p>Director, GCET</p>
+              </div></div>
+
     </div>
   );
 }
