6c19413067e8e06396e2a751afd3969ce38b0dd3
Vijay Singh <thakurvijayofficial@gmail.com>
2025-05-09 07:10:43 +0530

    Updated Sponsors

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,43 +1,45 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "../Assets/styles/Slider.css";
 
 const Slider = ({ slides }) => {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-    // Duplicate the slides array to ensure seamless looping
-    const duplicatedSlides = [...slides, ...slides];
+  // Duplicate the slides array to ensure seamless looping
+  const duplicatedSlides = [...slides, ...slides];
 
-    return (
-        <div className="relative w-full overflow-hidden">
-            {/* Wrapping div for seamless looping */}
-            <motion.div
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                className="flex"
-                animate={{
-                    x: ['0%', '-400%'],
-                    transition: {
-                        ease: 'linear',
-                        duration: 26, // Adjust the duration here (in seconds)
-                        repeat: Infinity,
-                    }
-                }}
-                onHoverStart={() => setIsHovered(true)}
-                onHoverEnd={() => setIsHovered(false)}
-                style={{ pointerEvents: isHovered ? 'none' : 'auto' }} // Disable pointer events when hovered
-            >
-                {/* Render duplicated slides */}
-                {duplicatedSlides.map((slide, index) => (
-                    <div key={index} className="flex-shrink-0 flex flex-col items-center justify-center" style={{ width: `${100 / slides.length * 4}%` }}>
-                        <img
-                            src={slide?.source}
-                            className=" scale-75"
-                        />
-                    </div>
-                ))}
-            </motion.div>
-        </div >
-    );
+  return (
+    <div className="relative w-full overflow-hidden">
+      {/* Wrapping div for seamless looping */}
+      <motion.div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="flex"
+        animate={{
+          x: ["0%", "-400%"],
+          transition: {
+            ease: "linear",
+            duration: 26, // Adjust the duration here (in seconds)
+            repeat: Infinity,
+          },
+        }}
+        onHoverStart={() => setIsHovered(true)}
+        onHoverEnd={() => setIsHovered(false)}
+        style={{ pointerEvents: isHovered ? "none" : "auto" }} // Disable pointer events when hovered
+      >
+        {/* Render duplicated slides */}
+        {duplicatedSlides.map((slide, index) => (
+          <div
+            key={index}
+            className="flex-shrink-0 flex flex-col items-center justify-center"
+            style={{ width: `${(100 / slides.length) * 4}%` }}
+          >
+            <img src={slide?.source} className="sponser-slider scale-75" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Slider;
--- src/Components/SponsersPartners.jsx
+++ src/Components/SponsersPartners.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import Heading from "../UI elements/Heading";
-import Button from "../UI elements/Button";
-import '../sliding.css'
+import "../sliding.css";
 import Slider from "./Slider";
 
 const SponsersPartners = () => {
   const slides = [
-    { source: "/images/Sponsers/ae.jpg" },
-    { source: "/images/Sponsers/aw.jpg" },
     { source: "/images/Sponsers/byjus.png" },
-    { source: "/images/Sponsers/cn.jpg" },
-    { source: "/images/Sponsers/ee.png" },
-    { source: "/images/Sponsers/idp.jpg" },
-    { source: "/images/Sponsers/ieee.png" },
-    { source: "/images/Sponsers/nis.jpg" },
     { source: "/images/Sponsers/pw.jpg" },
     { source: "/images/Sponsers/rb.jpg" },
-    { source: "/images/Sponsers/w.jpg" },
+    { source: "/images/Sponsers/royal_book.png" },
+    { source: "/images/Sponsers/coding_ninjas.webp" },
+    { source: "/images/Sponsers/GFG.png" },
+    { source: "/images/Sponsers/unstop.jpg" },
+    { source: "/images/Sponsers/zorko.jpg" },
   ];
   return (
     <div
@@ -24,47 +20,12 @@ const SponsersPartners = () => {
       id="sponsors"
     >
       <Heading index={"04"} title={"Sponsors and Partners"} />
-      {/* <div className="slider">
-        <div className="slide-track">
-          <img src="/images/Sponsers/images.png" className="slide" />
-          <img src="/images/Sponsers/images.png" className="slide" />
-        </div>
-      </div> */}
 
       <div className="font-mono text-md md:text-2xl mt-8 text-center">
-        Past Sponsors and Partners
+        Our Valued Sponsors & Partners
       </div>
       <Slider slides={slides} />
-
-
-
-      {/* <div className="grid w-full grid-cols-2 gap-6 my-6 rounded-md place-self-center lg:grid-cols-3"> */}
-      {/* {items.map((item) => (
-                    <div
-                        key={item.id}
-                        className="flex flex-col items-center justify-center w-full gap-3 p-6 overflow-hidden transition-all duration-700 border border-transparent rounded-md cursor-pointer select-none hover:scale-105 bg-neutral-700 h-52 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 hover:border-primary"
-                    >
-                        <div>
-                            <img
-                                src={item.imgSrc}
-                                className="object-contain w-full h-20 lg:h-24 drop-shadow-lg"
-                            />
-                        </div>
-                        <p className="text-lg font-medium text-center text-primary">
-                            {item.title}
-                        </p>
-                    </div>
-                ))} */}
-
-
-      <div className="flex flex-col gap-5 mt-5 items-center text-center">
-        <div className="font-mono text-md md:text-3xl w-full">
-          Looking for Sponsors and Partners
-        </div>
-        <a href="https://forms.gle/toWc8mR5kX5E5PX17" target="_blank"><Button title="Become Sponsor/ Partners" styles={'text-sm'} /></a>
-      </div>
     </div>
-    // </div>
   );
 };
 
